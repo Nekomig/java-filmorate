19b51f0021ea49cebb1f0d100e1cca4d530101b2
Sviatoslav Vakhtin <svyatoslav-vakhtin@yandex.ru>
2024-04-06 12:06:16 +0300

    fix idgen

--- src/main/java/ru/yandex/practicum/filmorate/services/IdGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/IdGenerator.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.services;
 
 public class IdGenerator {
-    private static int id = 0;
+    private static int id = 1;
 
     public static int getId() {
         return id++;
